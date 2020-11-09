a8fdeebe7c590c9b844eec5da48eed320f3b087b
Чернышенко Михаил Дмитриевич <Mikhail_Chernyshenko@epam.com>
2020-11-09 12:42:43 +0300

    refactor: eslint autofix

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://pp6vr2duqa.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://pp6vr2duqa.execute-api.eu-west-1.amazonaws.com/dev',
