bfb3027d5b5c3e7d6db2ea266196a5987498aab2
Чернышенко Михаил Дмитриевич <Mikhail_Chernyshenko@epam.com>
2020-11-19 19:44:09 +0300

    feat: import api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: 'https://pp6vr2duqa.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://8wykqdxli7.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://pp6vr2duqa.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
